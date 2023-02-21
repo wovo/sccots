5036d0854cb06b8fecc68197806deb20dded7e34
Wouter van Ooijen <wouter@voti.nl>
2023-02-21 10:00:39 +0100

    update

--- scripts/capture_camera.py
+++ scripts/capture_camera.py
@@ -19,5 +19,16 @@ ret, frame = cap.read()
 # write frame to file
 cv2.imwrite( "image.jpg", frame)
 
+# show in window
+if 1:
+ while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imshow('Input', frame)
+
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+
 # release camera
 cap.release()
