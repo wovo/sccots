5477ebfea7d6756d714701da84e4726204f1583d
Wouter van Ooijen <wouter@voti.nl>
2023-03-09 14:44:32 +0100

    update

--- dockers/generate.py
+++ dockers/generate.py
@@ -165,7 +165,7 @@ def main():
 
 if __name__ == "__main__":
     main()
-    print( "Docker files generated.\n"
-    print( "Don't forget to push to git.\n"
+    print( "Docker files generated.\n" )
+    print( "Don't forget to push to git.\n" )
 
 # ============================================================================   
